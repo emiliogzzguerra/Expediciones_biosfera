ff7315c879687b832aae3c02707adadcc74efc50
Javier De la Garza <javierdelagarza96@gmail.com>
2018-04-16 18:54:50 -0500

    Fix merge conflicts

--- app/src/main/java/itesm/mx/expediciones_biosfera/behavior/fragments/ProfileFragment.java
+++ app/src/main/java/itesm/mx/expediciones_biosfera/behavior/fragments/ProfileFragment.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import itesm.mx.expediciones_biosfera.R;
 
@@ -14,17 +16,34 @@ import itesm.mx.expediciones_biosfera.R;
  */
 public class ProfileFragment extends Fragment {
 
+    Button btn_change;
+    TextView tv_occupation;
+    TextView tv_interests;
+    TextView tv_phone;
 
     public ProfileFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        btn_change = view.findViewById(R.id.btn_change);
+        tv_occupation = view.findViewById(R.id.tv_occupation);
+        tv_interests = view.findViewById(R.id.tv_interests);
+        tv_phone = view.findViewById(R.id.tv_phone);
+
+        //los textview se deben de ver así
+        //Ocupacion: #####
+        //Intereses: #####
+        //Telefono: #####
+
+        //si quieren modificar los contenidos de los textviews usen este ejemplo
+        tv_occupation.setText(String.format(getResources().getString(R.string.occupation), "Estudiante"));
+
+        return view;
     }
 
 }
