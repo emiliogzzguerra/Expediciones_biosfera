d00531a65e0dfa5a64829e85dfc889ae2b5f5e9c
Hector Mg <hectormgwake@gmail.com>
2018-04-15 23:39:36 -0500

    Update project structure to add activities directory to src

--- app/src/main/java/itesm/mx/expediciones_biosfera/activities/MainActivity.java
+++ app/src/main/java/itesm/mx/expediciones_biosfera/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package itesm.mx.expediciones_biosfera;
+package itesm.mx.expediciones_biosfera.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import itesm.mx.expediciones_biosfera.R;
 
 public class MainActivity extends AppCompatActivity {
 
