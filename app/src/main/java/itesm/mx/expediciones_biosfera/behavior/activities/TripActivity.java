1d1c9e8ff7e537092904e0e16611ffb25abe7f5c
Adan Villarreal <adanvillarreal0@gmail.com>
2018-04-16 13:55:29 -0500

    Adds initial structure

--- app/src/main/java/itesm/mx/expediciones_biosfera/behavior/activities/TripActivity.java
+++ app/src/main/java/itesm/mx/expediciones_biosfera/behavior/activities/TripActivity.java
@@ -1,13 +1,20 @@
-package itesm.mx.expediciones_biosfera;
+package itesm.mx.expediciones_biosfera.behavior.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import itesm.mx.expediciones_biosfera.R;
+import itesm.mx.expediciones_biosfera.entities.models.Trip;
 
 public class TripActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Trip trip = new Trip("Titulo", null, 20, 500.13, 3, null, null);
+        
+        getActionBar().setTitle(trip.getTitle());
+
         setContentView(R.layout.activity_trip);
     }
 }
