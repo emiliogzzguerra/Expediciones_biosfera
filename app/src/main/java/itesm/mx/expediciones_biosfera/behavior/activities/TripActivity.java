026b9ebb783eb0d9ab1a258c846863289f3e19b3
Adan Villarreal <adanvillarreal0@gmail.com>
2018-04-16 15:26:37 -0500

    Basic structure for xml

--- app/src/main/java/itesm/mx/expediciones_biosfera/behavior/activities/TripActivity.java
+++ app/src/main/java/itesm/mx/expediciones_biosfera/behavior/activities/TripActivity.java
@@ -3,7 +3,10 @@ package itesm.mx.expediciones_biosfera.behavior.activities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.sql.Date;
+
 import itesm.mx.expediciones_biosfera.R;
+import itesm.mx.expediciones_biosfera.entities.models.Destination;
 import itesm.mx.expediciones_biosfera.entities.models.Trip;
 
 public class TripActivity extends AppCompatActivity {
@@ -11,7 +14,8 @@ public class TripActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Trip trip = new Trip("Titulo", null, 20, 500.13, 3, null, null);
+        Destination destination = new Destination("Parras", "Coahuila", "Parras", 25.442905, -102.176722, "Pueblo magico", null);
+        Trip trip = new Trip("Titulo", Date.valueOf("2015-15-01"), 20, 500.13, 3, destination, null);
         
         getActionBar().setTitle(trip.getTitle());
 
