705af0a0aaa40119f456380a00a18fce94c12ba1
Diego Astiazaran <diegoaat97@hotmail.com>
2018-04-16 20:50:33 -0500

    Navigation drawer working with placeholder fragments

--- app/src/main/java/itesm/mx/expediciones_biosfera/behavior/activities/DrawerActivity.java
+++ app/src/main/java/itesm/mx/expediciones_biosfera/behavior/activities/DrawerActivity.java
@@ -1,15 +1,97 @@
 package itesm.mx.expediciones_biosfera.behavior.activities;
 
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.ListFragment;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import itesm.mx.expediciones_biosfera.R;
+import itesm.mx.expediciones_biosfera.behavior.fragments.LogoutFragment;
+import itesm.mx.expediciones_biosfera.behavior.fragments.PackagesFragment;
+import itesm.mx.expediciones_biosfera.behavior.fragments.ProfileFragment;
 
-public class DrawerActivity extends AppCompatActivity {
+public class DrawerActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drawer);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        navigationView.setCheckedItem(R.id.nav_packages);
+
+        Menu menu = navigationView.getMenu();
+        for (int i = 0; i < menu.size(); i++) {
+            MenuItem item = menu.getItem(i);
+            if (item.isChecked()) {
+                getSupportActionBar().setTitle(item.getTitle());
+            }
+        }
+
+        PackagesFragment packagesFragment = new PackagesFragment();
+
+        getSupportFragmentManager().beginTransaction().add(R.id.content_frame, packagesFragment).commit();
+
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        Fragment fragment = null;
+
+        if (id == R.id.nav_packages) {
+            fragment = new PackagesFragment();
+
+
+        } else if (id == R.id.nav_profile) {
+            fragment = new ProfileFragment();
+
+        } else if (id == R.id.nav_signout) {
+            fragment = new LogoutFragment();
+
+        }
+
+        getSupportFragmentManager().popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE);
+
+        if (fragment != null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.content_frame, fragment).commit();
+            item.setChecked(true);
+            getSupportActionBar().setTitle(item.getTitle());
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
