7635afa46f86265a394ab295751e2507cf817165
Adan Villarreal <adanvillarreal0@gmail.com>
2018-05-03 13:29:23 -0500

    Adds functionality

--- app/src/main/java/itesm/mx/expediciones_biosfera/behavior/activities/DestinationActivity.java
+++ app/src/main/java/itesm/mx/expediciones_biosfera/behavior/activities/DestinationActivity.java
@@ -4,8 +4,10 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
@@ -33,6 +35,7 @@ import itesm.mx.expediciones_biosfera.entities.models.Trip;
 public class DestinationActivity extends AppCompatActivity implements OnMapReadyCallback {
     private GoogleMap mMap;
     FirebaseFirestore db;
+    ImageView ivPhoto;
     TextView tvLocation;
     TextView tvDate;
     TextView tvDescription;
@@ -51,6 +54,7 @@ public class DestinationActivity extends AppCompatActivity implements OnMapReady
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        ivPhoto = findViewById(R.id.iv_main_image);
         tvLocation = findViewById(R.id.tv_location);
         tvDate = findViewById(R.id.tv_date);
         tvDescription = findViewById(R.id.tv_description);
@@ -63,10 +67,16 @@ public class DestinationActivity extends AppCompatActivity implements OnMapReady
         if(bundle != null) {
             destination = (Destination) bundle.getSerializable("Destination");
         }
+            Glide.with(ivPhoto.getContext())
+                    .load("https://firebasestorage.googleapis.com/v0/b/expedicionesbiosfera.appspot.com/o/Cuatro%20cienegas%2Fcuatro-cienegas-coahuila-pueblo-magico.1600.jpg?alt=media&token=59c33c6d-ccbc-46a4-b6d1-16a0ea4c08ad")
+                    .placeholder(R.drawable.common_google_signin_btn_text_light)
+                    .dontAnimate()
+                    .into(ivPhoto);
             location = new LatLng(destination.getLat(), destination.getLon());
             tvLocation.setText(destination.getState() + ", " + destination.getCity());
             getSupportActionBar().setTitle(destination.getName());
-            tvDescription.setText(destination.getDescription());
+            tvDescription.setText(destination.getDescription().replaceAll("\\\\n", "\n"));
+
     }
 
     @Override
