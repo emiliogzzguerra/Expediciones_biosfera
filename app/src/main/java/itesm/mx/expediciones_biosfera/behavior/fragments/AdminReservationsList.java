993f55b5ac893a363fe8769f9011a44fe686d33d
Adan Villarreal <adanvillarreal0@gmail.com>
2018-05-04 11:31:09 -0500

    Adds class for reservation list

---
+++ app/src/main/java/itesm/mx/expediciones_biosfera/behavior/fragments/AdminReservationsList.java
@@ -0,0 +1,12 @@
+package itesm.mx.expediciones_biosfera.behavior.fragments;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by avillarreal on 5/4/18.
+ */
+
+public class AdminReservationsList extends Fragment {
+    private RecyclerView recyclerView;
+}
